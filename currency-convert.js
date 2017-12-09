74723b9fa6465e131e9f2876923c77357a5a626b
Pavan <pavandeshpande60@gmail.com>
2017-12-09 16:49:24 +0530

    currency-convert.js with error handlers

--- currency-convert.js
+++ currency-convert.js
@@ -1,16 +1,27 @@
 const axios = require('axios');
-const getExchangeRate = (from, to) => {
-    return axios.get(`https://api.fixer.io/latest?base=${from}`).then((response) => {
-        return response.data.rates[to];
-    });
+
+
+const getExchangeRate = async (from, to) => {
+    try {
+        const response = await axios.get(`https://api.fixer.io/latest?base=${from}`)
+        const rate = response.data.rates[to];
+        if (rate) {
+            return rate;
+        } else {
+            throw new Error();
+        }
+    } catch (e) {
+        throw new Error(`Unable to get exchange rate from ${from} and ${to}`);
+    }
 };
 
-const getCountries = (currencyCode) => {
-    return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((res) => {
-        return res.data.map((country) => {
-            return country.name;
-        })
-    });
+const getCountries = async (currencyCode) => {
+    try {
+        const res = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
+        return res.data.map((country) => country.name);
+    } catch (e) {
+        throw new Error(`Unable to find countries with currency code ${currencyCode}`)
+    }
 };
 
 
@@ -34,6 +45,8 @@ const convertCurrencyAlt = async (from, to, amount) => {
     return `${amount} ${from} is worth ${exchangedAmount} ${to}. \n${to} can be used in the following countries:-\n${countries.join(', ')}.`;
 };
 
-convertCurrencyAlt('USD', 'INR', 75000).then((amount) => {
+convertCurrencyAlt('USD', 'EUR', 5000).then((amount) => {
     console.log(amount);
+}).catch((e) => {
+    console.log(e.message);
 });
